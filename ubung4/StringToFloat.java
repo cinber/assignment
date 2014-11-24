ea61dea81e6e92f51d15c0b6c3faea5d5f6291f5
glor <glor@localhost.localdomain>
2014-11-24 16:47:31 +0100

    construct frame

--- ubung4/StringToFloat.java
+++ ubung4/StringToFloat.java
@@ -9,18 +9,27 @@ class StringToFloat
             System.exit(1);
         }
         String str = args[0];
+        int e = bin2int(str.substring(1,9)) - 127;
+        int m = bin2int(str.substring(9));
+        
 
-
-        char[] chars = args[0].toCharArray();
+        
         int s = (str.charAt(0) == '1') ? -1 : 1;
         //Boolean s = (str.charAt(0) == '1');
         for (int i = 1; i < 8; i++)
         {
             System.out.print(str.charAt(i));
         }
-
+        float f = e * Math.pow(2,e) * m;
+        System.out.println("s = " + s);
+        System.out.println("e = " + e);
+        System.out.println("m = " + m);
         System.out.println();
 
+    }
+    public static int bin2int(String s) {
+        System.out.println("substring: " + s);
+        return 0;
     }
 }
 
